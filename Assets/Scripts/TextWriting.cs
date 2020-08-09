25025cf8ec0336086b838f8c80a052888b952067
nice-shot <shefferu@gmail.com>
2020-08-09 20:55:26 +0300

    Added a text writing UI that changes color when a button is clicked.

---
+++ Assets/Scripts/TextWriting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriting : MonoBehaviour
+{
+    private string _originalText;
+    private int _originalTextIndex = 0;
+
+    public Text UIText;
+    [Range(0f, 1f)]
+    public float Value = 0f;
+
+    private void WriteLetters(int amount)
+    {
+        var colorClosingTag = "</color>";
+        // Remove color closing tag.
+        var currentText = UIText.text.Substring(0, UIText.text.Length - colorClosingTag.Length);
+
+        currentText += _originalText.Substring(_originalTextIndex, amount);
+        currentText += colorClosingTag;
+        UIText.text = currentText;
+
+        _originalTextIndex += amount;
+    }
+
+    private void Awake()
+    {
+        _originalText = UIText.text;
+        UIText.text = "";
+        // ChangeColor(UIText.color);
+    }
+
+    private IEnumerator Start()
+    {
+        foreach (var c in _originalText)
+        {
+            yield return new WaitForSeconds(0.01f);
+            WriteLetters(1);
+        }
+    }
+
+    public void ChangeColor(Color color)
+    {
+        UIText.text += $"<color=\"#{ColorUtility.ToHtmlStringRGB(color)}\"></color>";
+    }
+}
