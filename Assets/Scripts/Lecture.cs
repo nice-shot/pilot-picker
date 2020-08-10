517e0e4bd5da367dcb6520afcb68f65f2f9b3d4a
nice-shot <shefferu@gmail.com>
2020-08-10 12:04:54 +0300

    Created a flow generator and added a test for it.

--- Assets/Scripts/Lecture.cs
+++ Assets/Scripts/Lecture.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Lecture : MonoBehaviour
@@ -27,6 +29,36 @@ public class Lecture : MonoBehaviour
                 print($"{(item.Value as ScriptableObject).name} - {item.Key}");
             }
         }
+    }
+
+    public static SortedDictionary<float, int> GenerateFlow(
+        float duration,
+        Dictionary<int, float> distribution,
+        int seed=-1)
+    {
+        if (seed != -1) Random.InitState(seed);
+
+        var flow = new SortedDictionary<float, int>();
+        var currentTime = 0f;
+        var timedDistribution = distribution.ToDictionary(item => item.Key, item => item.Value * duration);
+        var minDuration = 2f;
+
+        while (currentTime < duration)
+        {
+            var styleIndex = Random.Range(0, timedDistribution.Keys.Count);
+            var maxDuration = timedDistribution[styleIndex];
+            if (maxDuration <= 0f) continue;
+
+            var styleDuration = Random.Range(
+                Mathf.Min(minDuration, maxDuration),
+                maxDuration
+            );
+            timedDistribution[styleIndex] -= styleDuration;
+            currentTime += styleDuration;
+            flow[currentTime] = styleIndex;
+        }
+
+        return flow;
     }
 
 }
---
+++ Assets/Scripts/Tests/LectureGeneratorTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class LectureTests
+    {
+        [Test]
+        public void Test_Lecture_GenerateFlow()
+        {
+            var styleDistribution = new Dictionary<int, float>()
+            {
+                { 0, 0.6f },
+                { 1, 0.2f },
+                { 2, 0.15f },
+                { 3, 0.05f },
+            };
+            var duration = 30f;
+            var flow = Lecture.GenerateFlow(duration, styleDistribution, 0);
+
+            var outputDistribution = new Dictionary<int, float>();
+            var prevKey = 0f;
+            foreach (var item in flow)
+            {
+                var addedDistributionPercentage = (item.Key - prevKey) / duration;
+
+                if (outputDistribution.ContainsKey(item.Value))
+                {
+                    outputDistribution[item.Value] += addedDistributionPercentage;
+                }
+                else
+                {
+                    outputDistribution[item.Value] = addedDistributionPercentage;
+                }
+                prevKey = item.Key;
+            }
+
+            Assert.AreEqual(styleDistribution.Count, outputDistribution.Count);
+            foreach (var item in styleDistribution)
+            {
+                Assert.AreEqual(item.Value, outputDistribution[item.Key], 0.0001f);
+            }
+        }
+    }
+}
