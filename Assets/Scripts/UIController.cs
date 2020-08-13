359685b8c8b66a3fcd1af324d55142565c09c958
nice-shot <shefferu@gmail.com>
2020-08-13 15:05:12 +0300

    Added ambient sound and small sounds for the ui.

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,18 +5,23 @@ public class UIController : MonoBehaviour
 {
     public GameObject ResultsContainer;
     public GameObject IntroMessageContainer;
+    public AudioSource PaperAudio;
+    public AudioClip PaperFastClip;
+    public AudioClip PaperSlowClip;
     public Text ScoreDisplayText;
 
     private void Awake()
     {
         Reset();
         IntroMessageContainer.SetActive(true);
+        if (PaperAudio) PaperAudio.PlayOneShot(PaperSlowClip);
     }
 
     public void Reset()
     {
         ResultsContainer.SetActive(false);
         IntroMessageContainer.SetActive(false);
+        if (PaperAudio) PaperAudio.PlayOneShot(PaperFastClip);
     }
 
     public void ShowScore(float score)
@@ -24,5 +29,6 @@ public class UIController : MonoBehaviour
         print($"Final score: {score}!");
         ResultsContainer.SetActive(true);
         ScoreDisplayText.text = score.ToString("0");
+        if (PaperAudio) PaperAudio.PlayOneShot(PaperSlowClip);
     }
 }
